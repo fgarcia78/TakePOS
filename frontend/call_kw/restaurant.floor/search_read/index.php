f73e4cba975cea8c9b148de948fbfe69bffc5120
andreubisquerra <jove@bisquerra.com>
2018-09-10 21:31:56 +0200

    Enable or disable Restaurant mode

--- frontend/call_kw/restaurant.floor/search_read/index.php
+++ frontend/call_kw/restaurant.floor/search_read/index.php
@@ -1,6 +1,10 @@
 <?php
+$res=@include("../../../../main.inc.php");
+if (! $res) $res=@include("../../../../../main.inc.php");
 header('Content-Type: application/json');
 $json_str = file_get_contents('php://input');
 $json_obj = json_decode($json_str);
+
+if($conf->global->TAKEPOS_BAR_RESTAURANT) echo'{"result": [{"background_color": "rgb(136,137,242)", "name": "Main Floor", "table_ids": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "sequence": 1, "id": 1}], "jsonrpc": "2.0", "id": 882565402}';
+else echo '{"result": [], "jsonrpc": "2.0", "id": 212460193}';
 ?>
-{"result": [{"name": "Kitchen Printer", "product_categories_ids": [1], "id": 1, "proxy_ip": "localhost"}], "jsonrpc": "2.0", "id": 212460193}
